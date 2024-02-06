34ea0e1e8fc29ed968d66f41a384e84d2aa8d10c
Alessio Prini <alessio.prini@gmail.com>
2024-02-06 11:54:21 +0100

    - update

--- deformable/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prini/Documents/pyDrake/pydrake_tests/deformable
-# Build directory: /home/prini/Documents/pyDrake/pydrake_tests/deformable/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
